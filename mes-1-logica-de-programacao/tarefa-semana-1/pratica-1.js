847f34aa9fb78298e27513f755904592e1194191
Igor Henrique <caladoigor010@hotmail.com>
2022-10-10 23:23:57 -0300

    resolvendo situação pratica-1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -8,11 +8,6 @@ email = "caladoigor010hotmail.com"
 profissão = "militar"
 idade = 19
 lugar = "Canadá"
-console.log(nome)
-console.log (email)
-console.log(profissão)
-console.log(idade)
-console.log(lugar)
 typeof(nome)
 typeof(email)
 typeof(profissão)
