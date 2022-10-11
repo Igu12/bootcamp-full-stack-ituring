2ea54586a5605fd6dc46b0ee49ab5713c7752d02
Igor Henrique <caladoigor010@hotmail.com>
2022-10-10 23:09:29 -0300

    modificar codigo pratica-1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -2,4 +2,9 @@ let nome
 let email
 let profissão
 let idade
-let lugar dos sonhos
+let lugar
+nome = "Igor Henrique"
+email = "caladoigor010hotmail.com"
+profissão = "militar"
+idade = 19
+lugar = "Canadá"
