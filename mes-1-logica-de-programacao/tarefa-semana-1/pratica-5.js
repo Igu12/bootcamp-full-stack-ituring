e69de26e7f7aa3153ab2d97471bd41adfd4f90a8
Igor Henrique <caladoigor010@hotmail.com>
2022-10-11 23:41:20 -0300

    adicionar pratica-5 no repositório

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -3,3 +3,4 @@ let email = "caladoigor010@hotmail.com"
 let profissão = "militar"
 let idade = 19
 let lugar = "Ilhas Maldivas"
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão}e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
