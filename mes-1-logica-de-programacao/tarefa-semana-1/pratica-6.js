18ab4e9f5661a14e7d5a8fa84edfbf5989292bea
Igor Henrique <caladoigor010@hotmail.com>
2022-10-11 23:54:10 -0300

    adicionar prática-6 no repositório

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,21 @@
+const1 = 123
+let numero1 = 1
+let numero2 = 2
+let numero3= 3
+let numero4= 4
+let numero5 = 5
+let numero6 = 6 
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+console.log(const1 * numero1)   
+console.log(const1 *= numero2) 
+console.log(const1 *= numero3) 
+console.log(const1 *= numero4) 
+console.log(const1 *= numero5)
+console.log(const1 *= numero6)
+console.log(const1 *= numero7)   
+console.log(const1 *= numero8)
+console.log(const1 *= numero9)
+console.log(const1 * numero10)   
