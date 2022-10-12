5d6721bba948b4b34b10a0f15ec3e8f01acb68ef
Igor Henrique <caladoigor010@hotmail.com>
2022-10-11 22:57:34 -0300

    adicionar prática-3.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,10 @@
+let nome = "Igor Henrique"
+let email = "caladoigor010@hotmail.com"
+let profissão = "militar"
+let idade = 19
+let lugar = "Ilhas Maldivas"
+console.log(typeof nome == "string")
+console.log (typeof email == "string")
+console.log (typeof profissão == "string")
+console.log (typeof idade == "string")
+console.log (typeof lugar == "string")
